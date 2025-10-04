1adb18728c6ec7c6556b1b7a6dfb01c97af8597e
Kace <lakacey03@gmail.com>
2025-10-04 06:08:38 -0400

    re-add combobox

--- ViewModels/TestComboBoxViewModel.cs
+++ ViewModels/TestComboBoxViewModel.cs
@@ -1,6 +1,41 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+
 namespace AvaloniaTest.ViewModels;
 
-public class TestComboBoxViewModel
+public class TestComboBoxViewModel : ViewModelBase
 {
+    private ObservableCollection<string> _fruits = new ObservableCollection<string>
+    {
+        "Pineapple", "Banana", "Cherry", "Dragonfruit",
+        "Elderberry", "Fig", "Grapes", "Honeydew"
+    };
+    
+    public ObservableCollection<string> Fruits
+    {
+        get => _fruits;
+        set => this.RaiseAndSetIfChanged(ref _fruits, value);
+    }
+    
+    private string? _selectedFruit;
+    public string? SelectedFruit
+    {
+        get => _selectedFruit;
+        set => this.RaiseAndSetIfChanged(ref _selectedFruit, value);
+    }
+    
+     
+
+    public TestComboBoxViewModel()
+    {
+        Fruits = new ObservableCollection<string>
+        {
+            "Pineapple", "Banana", "Cherry", "Dragonfruit",
+            "Elderberry", "Fig", "Grapes", "Honeydew"
+        };
+    }
+        
+    
     
 }
